5fefe4cadd3c0c61aa0936120cd7d5b6e986f55e
Akrem <akremsalhi2@gmail.com>
2021-11-27 13:41:11 +0100

    fix: search

--- src/FetchingData.tsx
+++ src/FetchingData.tsx
@@ -7,6 +7,8 @@ import UsersSkeleton from './UsersSkeleton';
 import Fetch from './utils/http/components/Fetch';
 import FormSearchInput from './UI/Components/Forms/FormSearchInput';
 import { FormFetch } from './shared/context/FormContext';
+import { List, ListIcon, ListItem } from '@chakra-ui/layout';
+import { MdCheckCircle } from "react-icons/md"
 
 
 export default function FetchingData() {
@@ -25,7 +27,7 @@ export default function FetchingData() {
     return (
         <div className="grid grid-cols-1 gap-y-2">
             <div className="grid justify-between items-center lg:grid-cols-2 sm:grid-cols-1 xs:grid-cols-1 sm:gap-y-1">
-                <h1 className="text-2xl">Fetching data</h1>
+                <h1 className="text-2xl">Example REST API with <strong>React</strong></h1>
 
                 <div className="grid lg:grid-cols-2 md:grid-cols-1 sm:grid-cols-1 sm:gap-y-2 xs:grid-cols-1 xs:gap-y-2 justify-between items-center gap-x-2">
 
@@ -45,6 +47,24 @@ export default function FetchingData() {
                     {(users: any[]) =>  <Users users={users} />}
                 </Fetch>
             </FormFetch>
+
+            <div className="mt-10 grid grid-cols-1 gap-y-3">
+                <strong>Important:</strong>
+                <List spacing={3}>
+                    <ListItem className="text-lg">
+                        <ListIcon as={MdCheckCircle} color="green.500" />
+                        Catching http errors.
+                    </ListItem>
+                    <ListItem className="text-lg">
+                        <ListIcon as={MdCheckCircle} color="green.500" />
+                        Show content as the request data is ready.
+                    </ListItem>
+                    <ListItem className="text-lg">
+                        <ListIcon as={MdCheckCircle} color="green.500" />
+                        Make the Search input as readonly if the data is not ready.
+                    </ListItem>
+                </List>
+            </div>
 
 
         </div>
