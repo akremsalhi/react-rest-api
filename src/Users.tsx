3700aa0f1031ea505c5c090eb7cce2ccd9794d83
Akrem <akremsalhi2@gmail.com>
2021-11-27 15:54:09 +0100

    fix: search

--- src/Users.tsx
+++ src/Users.tsx
@@ -8,7 +8,7 @@ interface UsersProps {
 export default function Users({ users }: UsersProps) {
     return (
         <div className="grid gap-2 lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1">
-            {users.map((user, index) => <SlideFade key={index} in offsetX="-20px" transition={{ enter: { duration: 0.09 * index }}}>
+            {users.map((user, index) => <SlideFade key={index} in offsetX="-20px" transition={{ enter: { duration: 0.1 * index }}}>
                 <Card title={user.name} content={user.address.street} />
             </SlideFade>)}
         </div>
